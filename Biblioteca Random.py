172d1d9375c48966ce3d0040f2009470f8fe2e63
tecnico54 <115370228+tecnico54@users.noreply.github.com>
2023-09-01 18:12:13 -0500

    Add files via upload

---
+++ Biblioteca Random.py
@@ -0,0 +1,8 @@
+def Biblioteca_Random(NUM):
+    NUM = [1, 2, 3, 4, 5, 6, 7]
+    return [1, 2, 3, 4, 5, 6, 7]
+NUM = [1, 2, 3, 4, 5, 6, 7]
+import numpy as np
+numero = np.random.randint(-1, 100)
+print(f"Los Datos Son: {NUM}")
+print(f"El número aleatorio es: {numero}")
